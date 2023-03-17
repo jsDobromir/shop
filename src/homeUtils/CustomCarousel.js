7a17ac3f6c00d08ac5f3a8484609166602d442ee
Dobromir Ganchev <dobrganch89@gmail.com>
2023-03-17 13:34:49 +0200

    image update

--- src/homeUtils/CustomCarousel.js
+++ src/homeUtils/CustomCarousel.js
@@ -30,34 +30,7 @@ export default class CustomCarousel {
             }
             //load next two images
             //preload the image
-            let image1 = (nextElement && nextElement.nextElementSibling) ? nextElement.nextElementSibling.querySelector('.imgClass') : undefined;
-            if (image1) {
-                let srcset = image1.dataset.srcset;
-                let imagesArray =srcset.split(',');
-                imagesArray = imagesArray.map(img => {
-                    return img.split(' ')[0];
-                });
-                console.log(imagesArray);
-                for (let i=0;i<imagesArray.length;i++) {
-                    const loadedimage = new Image();
-                    loadedimage.src = imagesArray[i];
-                }
-                image1.srcset = srcset;
-            }
-            let image2 = (nextElement && nextElement.nextElementSibling && nextElement.nextElementSibling.nextElementSibling) ? nextElement.nextElementSibling.nextElementSibling.querySelector('.imgClass') : undefined;
-            if (image2) {
-                let srcset = image2.dataset.srcset;
-                let imagesArray =srcset.split(',');
-                imagesArray = imagesArray.map(img => {
-                    return img.split(' ')[0];
-                });
-                console.log(imagesArray);
-                for (let i=0;i<imagesArray.length;i++) {
-                    const loadedimage = new Image();
-                    loadedimage.src = imagesArray[i];
-                }
-                image2.srcset = srcset;
-            }
+
             let xPos = nextElement.getBoundingClientRect().x;
             let currentXposition = this.carouselInner.getBoundingClientRect().x;
             let newPosition = currentXposition - xPos;
@@ -70,7 +43,7 @@ export default class CustomCarousel {
             }
             return;
         });
-        this.sliderManager.on('swiperight',  (e) => {
+        this.sliderManager.on('swiperight', (e) => {
             let activeElement = this.carouselInner.querySelector('.active');
             let prevElement = null;
             if (this.items_to_slide === 1) {
@@ -117,7 +90,7 @@ export default class CustomCarousel {
             }
             return;
         });
-    
+
         this.carouselInner.parentElement.parentElement.querySelector('.carousel-control-next').addEventListener('click', (ev) => {
             let activeElement = this.carouselInner.querySelector('.active');
             let nextElement = null;
@@ -131,33 +104,34 @@ export default class CustomCarousel {
             //load next two images
             //preload the image
             let image1 = (nextElement && nextElement.nextElementSibling) ? nextElement.nextElementSibling.querySelector('.imgClass') : undefined;
-            if (image1) {
-                let srcset = image1.dataset.srcset;
-                const loadedimage = new Image();
-                loadedimage.srcset = srcset;
-                loadedimage.onload = () => console.log('image loaded');
-                image1.srcset = srcset;
-            }
+            let promises = [];
+            if (image1) promises.push(this.loadImage(image1));
             let image2 = (nextElement && nextElement.nextElementSibling && nextElement.nextElementSibling.nextElementSibling) ? nextElement.nextElementSibling.nextElementSibling.querySelector('.imgClass') : undefined;
-            if (image2) {
-                let srcset = image2.dataset.srcset;
-                const loadedimage = new Image();
-                loadedimage.srcset = srcset;
-                loadedimage.onload = () => console.log('image loaded');
-                image2.srcset = srcset;
-            }
+            if (image2) promises.push(this.loadImage(image2));
             //
-            let xPos = nextElement.getBoundingClientRect().x;
-            let currentXposition = this.carouselInner.getBoundingClientRect().x;
-            let newPosition = currentXposition - xPos;
-            this.currentPosition = newPosition;
-            this.carouselInner.style.transform = 'translateX(' + (newPosition) + 'px)';
-            if (this.items_to_slide === 1) {
-                if (!nextElement.nextElementSibling) {
-                    this.carouselInner.parentElement.parentElement.querySelector('.carousel-control-next').classList.add('d-none');
+            Promise.all(promises).then(() => {
+                let xPos = nextElement.getBoundingClientRect().x;
+                let currentXposition = this.carouselInner.getBoundingClientRect().x;
+                let newPosition = currentXposition - xPos;
+                this.currentPosition = newPosition;
+                this.carouselInner.style.transform = 'translateX(' + (newPosition) + 'px)';
+                if (this.items_to_slide === 1) {
+                    if (!nextElement.nextElementSibling) {
+                        this.carouselInner.parentElement.parentElement.querySelector('.carousel-control-next').classList.add('d-none');
+                    }
                 }
-            }
-            return;
+                return;
+            })
         });
+    }
+
+    loadImage(image) {
+        return new Promise((resolve, reject) => {
+            let src = image.dataset.src;
+            image.src = src;
+            image.onload = () => {
+                return resolve();
+            }
+        })
     }
 }
