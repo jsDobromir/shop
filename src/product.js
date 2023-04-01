52c5481a98e8761d89614064d9a6a769948a52fd
Dobromir Ganchev <dobrganch89@gmail.com>
2023-04-01 17:21:06 +0300

    clean car

--- src/product.js
+++ src/product.js
@@ -49,72 +49,24 @@ document.addEventListener('DOMContentLoaded', (event) => {
         });
     });
 
-    const carouselImgWrappers = document.querySelectorAll('.carousel_img_wrapper');
+    const carouselImages = document.querySelectorAll('.carousel-item img');
+    carouselImages.forEach((image) => {
+        const hammer = new Hammer(image);
+        hammer.get('pinch').set({ enable: true });
 
-    const mc = new Hammer.Manager(document.body);
-    mc.add(new Hammer.Pinch());
-    mc.add(new Hammer.Pan({ direction: Hammer.DIRECTION_ALL, threshold: 0 }));
+        let lastScale = 1;
 
-    let lastScale = 1;
-    let lastPosX = 0;
-    let lastPosY = 0;
-    let isDragging = false;
-
-    carouselImgWrappers.forEach((wrapper) => {
-        const imgEl = wrapper.querySelector('img');
-        const checkboxEl = wrapper.querySelector('input[type="checkbox"]');
-
-        // disable checkbox click event while zooming
-        let isPinching = false;
-        mc.on('pinchstart', () => {
-            isPinching = true;
-        });
-        mc.on('pinchend', () => {
-            isPinching = false;
-        });
-        checkboxEl.addEventListener('click', (e) => {
-            if (isPinching) {
-                e.preventDefault();
-            }
+        hammer.on('pinchstart', () => {
+            lastScale = 1;
         });
 
-        // enable pinch-to-zoom and panning
-        mc.on('pinchmove', (e) => {
-            const scale = Math.max(1, Math.min(lastScale * e.scale, 3));
-            imgEl.style.transform = `scale(${scale}) translate(${lastPosX}px, ${lastPosY}px)`;
-        });
-        mc.on('panstart', () => {
-            isDragging = true;
-        });
-        mc.on('panmove', (e) => {
-            if (!isDragging || lastScale === 1) {
-                return;
-            }
-            const { deltaX, deltaY } = e;
-            lastPosX += deltaX;
-            lastPosY += deltaY;
-            imgEl.style.transform = `scale(${lastScale}) translate(${lastPosX}px, ${lastPosY}px)`;
-        });
-        mc.on('panend', () => {
-            isDragging = false;
+        hammer.on('pinchmove', (event) => {
+            const scale = Math.max(1, Math.min(lastScale * event.scale, 3));
+            image.style.transform = `scale(${scale})`;
         });
 
-        // reset zoom and position on checkbox change
-        checkboxEl.addEventListener('change', () => {
-            if (checkboxEl.checked) {
-                imgEl.style.transform = '';
-                lastScale = 1;
-                lastPosX = 0;
-                lastPosY = 0;
-            }
-        });
-
-        // enable scrolling when not zoomed in
-        wrapper.addEventListener('wheel', (e) => {
-            if (lastScale === 1) {
-                e.preventDefault();
-                wrapper.scrollLeft += e.deltaY * 0.25;
-            }
+        hammer.on('pinchend', () => {
+            lastScale = image.style.transform.replace('scale(', '').replace(')', '') || 1;
         });
     });
 
