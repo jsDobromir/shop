86fc4895c8e8a663d8e78f6789cb4cdff48d9a87
Dobromir Ganchev <dobrganch89@gmail.com>
2023-04-15 19:59:16 +0300

    upload

--- src/home.js
+++ src/home.js
@@ -10,21 +10,84 @@ document.addEventListener('DOMContentLoaded', (ev) => {
     const spaceBetweenRem = 1.3; // space between slides in rem
     const spaceBetweenPixels = parseFloat(getComputedStyle(document.documentElement).fontSize) * spaceBetweenRem;
     
+    const spaceBetweenRemLg = 2;
+    const spaceBetweenPixelsLg = parseFloat(getComputedStyle(document.documentElement).fontSize) * spaceBetweenRemLg;
+
+    const spaceBetweenRemXL = 3;
+    const spaceBetweenPixelsXL = parseFloat(getComputedStyle(document.documentElement).fontSize) * spaceBetweenRemXL;
+
     var swiper1 = new Swiper(".swiper-container-cosmetics", {
         slidesPerView: 'auto',
-        spaceBetween: spaceBetweenPixels, 
+        spaceBetween: spaceBetweenPixels,
+        breakpoints: {
+            576: {
+                slidesPerView: 2
+            },
+            768: {
+                slidesPerView: 'auto'
+            },
+            992: {
+                slidesPerView: 3,
+                spaceBetween: spaceBetweenPixelsLg
+            },
+            1200: {
+                slidesPerView: 'auto'
+            },
+            1400: {
+                slidesPerView: 4,
+                spaceBetween: spaceBetweenPixelsXL
+            }
+        }
     });
     
     var swiper2 = new Swiper(".swiper-container-supplements", {
         slidesPerView: 'auto',
         spaceBetween: 10,
-        spaceBetween: spaceBetweenPixels
+        spaceBetween: spaceBetweenPixels,
+        breakpoints: {
+            576: {
+                slidesPerView: 2
+            },
+            768: {
+                slidesPerView: 'auto'
+            },
+            992: {
+                slidesPerView: 3,
+                spaceBetween: spaceBetweenPixelsLg
+            },
+            1200: {
+                slidesPerView: 'auto'
+            },
+            1400: {
+                slidesPerView: 4,
+                spaceBetween: spaceBetweenPixelsXL
+            }
+        }
     });
 
     var swiper3 = new Swiper(".swiper-container-vitamins", {
         slidesPerView: 'auto',
         spaceBetween: 10,
-        spaceBetween: spaceBetweenPixels
+        spaceBetween: spaceBetweenPixels,
+        breakpoints: {
+            576: {
+                slidesPerView: 2
+            },
+            768: {
+                slidesPerView: 'auto'
+            },
+            992: {
+                slidesPerView: 3,
+                spaceBetween: spaceBetweenPixelsLg
+            },
+            1200: {
+                slidesPerView: 'auto'
+            },
+            1400: {
+                slidesPerView: 4,
+                spaceBetween: spaceBetweenPixelsXL
+            }
+        }
     });
 
     // let carouselXl = buildCarousel('carousel_xl', 'cosmetics');
