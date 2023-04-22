02dc2b1b7576487ec296ffd82bf6f470691650ea
Dobromir Ganchev <dobrganch89@gmail.com>
2023-04-22 16:58:55 +0300

    header

--- src/homeUtils/utils.js
+++ src/homeUtils/utils.js
@@ -1,15 +1,15 @@
 function spaceBetweenRemUtil() {
     const spaceBetweenRem = 1.3; // space between slides in rem
-    const spaceBetweenPixels = parseFloat(getComputedStyle(document.documentElement).fontSize) * spaceBetweenRem;
+    const spaceBetweenPixels = 10;
     
     const spaceBetweenRemMd = 2;
-    const spaceBetweenPixelsMD = parseFloat(getComputedStyle(document.documentElement).fontSize) * spaceBetweenRemMd;
+    const spaceBetweenPixelsMD = 13;
 
     const spaceBetweenRemLg = 2.4;
-    const spaceBetweenPixelsLg = parseFloat(getComputedStyle(document.documentElement).fontSize) * spaceBetweenRemLg;
+    const spaceBetweenPixelsLg = 16;
 
     const spaceBetweenRemXL = 3;
-    const spaceBetweenPixelsXL = parseFloat(getComputedStyle(document.documentElement).fontSize) * spaceBetweenRemXL;
+    const spaceBetweenPixelsXL = 19;
 
     return {
         spaceBetweenPixels,
