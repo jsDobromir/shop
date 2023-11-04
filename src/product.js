43f7bf619e2a89b21300cd12b31830f2327bc135
Dobromir Ganchev <dobrganch89@gmail.com>
2023-11-04 15:56:58 +0200

    updating project

--- src/product.js
+++ src/product.js
@@ -1,126 +1,24 @@
-import { Carousel } from "bootstrap";
 import { quantityListener, transListener, attachSlideListener, zoomIn, zoomOut} from "./productUtils/utils.js";
 import { productGalleryHash, initialGalleryHash } from './productUtils/hiddenCarouselUtils.js';
-import { renderer } from "./productUtils/renderer.js";
-import Hammer from "hammerjs";
+import {Swiper, Pagination} from "swiper";
 
 document.addEventListener('DOMContentLoaded', (event) => {
-
-    const carouselDom = document.querySelector(`.container__prod #imageCarousel`);
-    const carousel = new Carousel(carouselDom, {
-        interval: 5000,
-        keyboard: true,
-        pause: false,
-        ride: false,
-        touch: true,
-        wrap: true
-    });
-
-
-    const hiddenCarouselDom = document.querySelector('.product_gallery_div #hiddenCarousel');
-    const carouselHidden = new Carousel(hiddenCarouselDom, {
-        interval: 5000,
-        keyboard: true,
-        pause: false,
-        ride: false,
-        touch: false,
-        wrap: true
-    });
-
-    carouselDom.addEventListener('slide.bs.carousel', (event) => {
-        const slideIndex = event.to;
-        carouselHidden.to(slideIndex);
-    });
-
-    let overlay = document.querySelector('#hiddenCarousel .carousel-item.active .carousel_img_wrapper');
-    overlay.viewer = null;
-
-    overlay.viewer = renderer({
-        minScale: 1,
-        maxScale: 4,
-        element: overlay,
-        scaleSensitivity: 10
-    });
-
-    hiddenCarouselDom.addEventListener('slid.bs.carousel', (event) => {
-        const slideIndex = event.to;
-        carousel.to(slideIndex);
-
-        overlay = document.querySelector('#hiddenCarousel .carousel-item.active .carousel_img_wrapper');
-        overlay.viewer = null;
-        overlay.viewer = renderer({
-            minScale: 1,
-            maxScale: 4,
-            element: overlay,
-            scaleSensitivity: 10
-        });
-        const carouselItems = document.querySelectorAll('#hiddenCarousel .carousel-item');
-
-        carouselItems.forEach((item) => {
-            // Get the corresponding checkbox
-            const checkbox = item.querySelector('input[type="checkbox"]');
-
-            // Check if the carousel item has the active class
-            if (checkbox && !item.classList.contains('active')) {
-                // If it has the active class, leave the checkbox checked
-                checkbox.checked = false;
-            }
-        });
-    });
-
-    let isZoomedIn = false;
-
-    const hiddenCaroselMan = hiddenCarouselDom.querySelector('.carousel-inner');
-    let hammerTime = new Hammer(hiddenCaroselMan);
-    hammerTime.on('swipeleft', (event) => {
-        console.log('swipeleft');
-        if (!isZoomedIn) {
-            carouselHidden.next();
+    Swiper.use([Pagination]);
+    
+    const swiper = new Swiper('.flipSwiper', {
+        effect: "cube",
+        grabCursor: true,
+        cubeEffect: {
+          shadow: true,
+          slideShadows: true,
+          shadowOffset: 20,
+          shadowScale: 0.94,
+        },
+        pagination: {
+            el: '.swiper-pagination'
         }
     });
-
-    hammerTime.on('swiperight', (event) => {
-        console.log('right');
-        if (!isZoomedIn) {
-            carouselHidden.prev();
-        }
-    });
-
-    document.querySelector('.zoom-in-icon').addEventListener('click', (event) => {
-        event.preventDefault();
-        let carousel_item_parent = hiddenCarouselDom.querySelector('.carousel-item.active');
-        let img = carousel_item_parent.querySelector('img');
-        if (!isZoomedIn) {
-            let elem = document.querySelector('.zoom-in-icon-span');
-            elem.classList.remove('zoom-in-icon-back');
-            elem.classList.add('zoom-out-icon-back');
-            zoomIn(overlay, carousel_item_parent, img);
-            isZoomedIn = true;
-        } else {
-            zoomOut(overlay, carousel_item_parent, img);
-            let elem = document.querySelector('.zoom-in-icon-span');
-            elem.classList.remove('zoom-out-icon-back');
-            elem.classList.add('zoom-in-icon-back');
-            isZoomedIn = false;
-        }
-    });
-
-    document.querySelectorAll('#hiddenCarousel .carousel-item img').forEach((img) => {
-        console.log(isZoomedIn);
-        img.addEventListener('click', (event) => {
-            let carousel_item_parent = img.closest('.carousel-item');
-            if (!isZoomedIn) {
-                zoomIn(overlay, carousel_item_parent, img);
-                isZoomedIn = true;
-            } else {
-                zoomOut(overlay, carousel_item_parent, img);
-                isZoomedIn = false;
-            }
-        });
-    });
-
-
-
+    
     quantityListener();
 
     transListener();
--- upload.js
+++ upload.js
@@ -1,37 +1,70 @@
 require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
+const fs = require('fs');
+let imageNum = 0;
+let urls = [];
 
-cloudinary.uploader.upload("./dist/prod_images/prod11/resp_xl.jpg", {folder: "prod11", public_id: "prod11_front", responsive_breakpoints: {create_derived: true, bytes_step: 20000, min_width: 200, max_width: 501, transformation: {crop: 'fit'}}}, function(error, result) {
-    let breakpoints = (result.responsive_breakpoints);
-    for(let br of breakpoints) {
-        console.log(br);
-    }
-})
+cloudinary.uploader.upload('./dist/prod_images/prod79/small_images/image_1.webp', {
+    folder: 'prod79/small_images',
+    public_id: `image_1`
+    // responsive_breakpoints: {
+    //     create_derived: false,
+    //     min_width: 50,
+    //     max_width: 1400,
+    //     fetch_format: 'auto'
+    // }
+}, (error, result) => {
+    // let breakpoints = result.responsive_breakpoints;
+    // let localUrl = [];
+    // for (let br of breakpoints[0].breakpoints) {
+    //     localUrl.push(br.secure_url);
+    // }
+    // urls.push(localUrl);
+    console.log(result);
+});
 
-// function uploadImage(image, options) {
+//let images = ['./dist/prod_images/prod3/pdp/normal/image_1.jpg', './dist/prod_images/prod3/pdp/normal/image_2.jpg', './dist/prod_images/prod3/pdp/normal/image_3.jpg', './dist/prod_images/prod3/pdp/normal/image_4.jpg', './dist/prod_images/prod3/pdp/normal/image_5.jpg', './dist/prod_images/prod3/pdp/normal/image_6.jpg'];
+
+
+// Promise.all(images.map(image => {
 //     return new Promise((resolve, reject) => {
-//         cloudinary.uploader.upload(image, options)
-//             .then(resulte => {
-//                 return resolve();
-//             })
-//             .catch(error => {
-//                 return reject();
-//             });
+//         imageNum++;
+//         cloudinary.uploader.upload(image, {
+//             folder: 'prod79/pdp/normal',
+//             public_id: `image_${imageNum.toString()}`,
+//             responsive_breakpoints: {
+//                 create_derived: false,
+//                 min_width: 50,
+//                 max_width: 1200,
+//                 fetch_format: 'auto'
+//             }
+//         }, (error, result) => {
+//             if (error) {
+//                 reject(error);
+//             } else {
+//                 let breakpoints = result.responsive_breakpoints;
+//                 let localUrl = [];
+//                 for (let br of breakpoints[0].breakpoints) {
+//                     localUrl.push(br.secure_url);
+//                 }
+//                 urls.push(localUrl);
+//                 resolve(urls);
+//             }
+//         });
 //     });
-// }
-
-// let promises = [
-//     uploadImage("./dist/prod_images/prod11/pdp/image_1.jpg", {folder: 'prod11/pdp', public_id: 'pic_pdp_1'}),
-//     uploadImage("./dist/prod_images/prod11/pdp/image_2.jpg", {folder: 'prod11/pdp', public_id: 'pic_pdp_2'}),
-//     uploadImage("./dist/prod_images/prod11/pdp/image_3.jpg", {folder: 'prod11/pdp', public_id: 'pic_pdp_3'}),
-//     uploadImage("./dist/prod_images/prod11/pdp/image_4.jpg", {folder: 'prod11/pdp', public_id: 'pic_pdp_4'}),
-//     uploadImage("./dist/prod_images/prod11/pdp/image_5.jpg", {folder: 'prod11/pdp', public_id: 'pic_pdp_5'}),
-//     uploadImage("./dist/prod_images/prod11/pdp/image_6.jpg", {folder: 'prod11/pdp', public_id: 'pic_pdp_6'}),
-// ];
-
-// Promise.all(promises).then(res => {
-//     console.log('images uploaded');
-// }).catch(err => {
-//     console.log(err);
-//     console.log('error uploading images');
+// })).then(() => {
+//     console.log(urls);
+//     const modifiedArray = urls.map(subArray => {
+//         return subArray.map(str => {
+//             const width = str.match(/w_(\d+)/)[1];
+//             return `${str} ${width}w`;
+//         });
+//     });
+//     const strings = modifiedArray.map(subArr => subArr.reverse().join(','));
+//     fs.writeFile('output.txt', strings.join('\n'), err => {
+//         if (err) throw err;
+//         console.log('File written successfully');
+//     });
+// }).catch(error => {
+//     console.log(error);
 // });
